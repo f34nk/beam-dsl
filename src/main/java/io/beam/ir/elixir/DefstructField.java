84104d13b07f058cc38c0158eacb6a535ac98c9d
Frank Eickhoff <info@frankeickhoff.de>
2026-07-06 14:19:42 +0200

    feat(elixir): add default values to DefstructField

--- src/main/java/io/beam/ir/elixir/DefstructField.java
+++ src/main/java/io/beam/ir/elixir/DefstructField.java
@@ -1,13 +1,18 @@
 package io.beam.ir.elixir;
 
-public record DefstructField(String nameOrNil, SourceSpan source) implements Node {
+public record DefstructField(String nameOrNil, Expression defaultOrNull, SourceSpan source)
+    implements Node {
 
   public static DefstructField field(String name) {
-    return new DefstructField(name, null);
+    return new DefstructField(name, null, null);
+  }
+
+  public static DefstructField field(String name, Expression defaultValue) {
+    return new DefstructField(name, defaultValue, null);
   }
 
   public static DefstructField nilField() {
-    return new DefstructField(null, null);
+    return new DefstructField(null, null, null);
   }
 
   @Override
