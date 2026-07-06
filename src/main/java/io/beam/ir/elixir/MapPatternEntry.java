12e2622901f23b60eed45d35e74132484f130820
Frank Eickhoff <info@frankeickhoff.de>
2026-07-06 14:19:42 +0200

    chore(elixir): remove unused import from MapPatternEntry

--- src/main/java/io/beam/ir/elixir/MapPatternEntry.java
+++ src/main/java/io/beam/ir/elixir/MapPatternEntry.java
@@ -1,7 +1,5 @@
 package io.beam.ir.elixir;
 
-import java.util.List;
-
 public record MapPatternEntry(Expression key, Pattern value, SourceSpan source) implements Node {
 
   public static MapPatternEntry of(Expression key, Pattern value) {
